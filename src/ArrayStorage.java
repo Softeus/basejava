68301b13f3ae21881b6810a7fd1071821ef19b04
Softeus <66733741+Softeus@users.noreply.github.com>
2022-03-19 13:28:50 +0300

    Realized save, get, delete, clear, getAll, size in ArrayStorage

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -1,30 +1,50 @@
+import java.util.Arrays;
+import java.util.Objects;
+
 /**
  * Array based storage for Resumes
  */
 public class ArrayStorage {
     Resume[] storage = new Resume[10000];
+    int numberOfResumes = 0;
 
     void clear() {
+        Arrays.fill(storage, null);
+        numberOfResumes = 0;
     }
 
     void save(Resume r) {
+        storage[numberOfResumes++] = r;
     }
 
     Resume get(String uuid) {
+        for (int i = 0; i < numberOfResumes; i++) {
+            if (Objects.equals(storage[i].uuid, uuid)) {
+                return storage[i];
+            }
+        }
         return null;
     }
 
     void delete(String uuid) {
+        for (int i = 0; i < numberOfResumes; i++) {
+            if (Objects.equals(storage[i].uuid, uuid)) {
+                numberOfResumes--;
+                System.arraycopy(storage, i + 1, storage, i, numberOfResumes - i);
+                storage[numberOfResumes] = null;
+                break;
+            }
+        }
     }
 
     /**
      * @return array, contains only Resumes in storage (without null)
      */
     Resume[] getAll() {
-        return new Resume[0];
+        return Arrays.copyOf(storage, numberOfResumes);
     }
 
     int size() {
-        return 0;
+        return numberOfResumes;
     }
 }
